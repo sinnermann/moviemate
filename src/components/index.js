1f0b4ae19c9d6ad5a48ac19bc8463d57f690f2e0
sinnermann <ealijev@gmail.com>
2023-10-11 18:44:31 +0400

    Complete Project

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+import { Card } from "./Card";
---
+++ src/pages/MovieList.js
@@ -0,0 +1,20 @@
+import { Card } from "../components/Card";
+import { useFetch } from "../hooks/useFetch";
+
+const MovieList = ({ apiPath }) => {
+  const { data: movies } = useFetch(apiPath);
+
+  return (
+    <main>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap other:justify-evenly">
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default MovieList;
